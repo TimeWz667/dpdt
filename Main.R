b95d2bb5c63873959edd040d07dfc66dc57a5c09
Chu Chang Ku <cm1cck@sheffield.ac.uk>
2020-01-22 16:37:57 +0000

    Add data preparation to main.R

--- Main.R
+++ Main.R
@@ -1,9 +1,4 @@
 
-
-dat_birth <- read.csv("Input/WPP2019_Fertility_by_Age.csv")
-dat_death <- read.csv("Input/WPP2019_Life_Table.csv")
-dat_pop <- read.csv("Input/WPP2019_PopulationByAgeSex.csv")
-
-
+# source("DataPreparation.R")
 
 
